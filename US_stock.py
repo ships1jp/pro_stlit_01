f08be7e98247b56cc52090578b94d33c27b83892
ships1jp <tsukabiz3000@gmail.com>
2021-09-12 18:37:17 +0900

    1st_commit

--- US_stock.py
+++ US_stock.py
@@ -3,18 +3,18 @@ import yfinance as yf
 import altair as alt
 import streamlit as st
 
-st.title('米国株価可視化アプリ')
+st.title('株価可視化アプリ            by t.tsuka')
 
 st.sidebar.write("""
 # US株価
-こちらは株価可視化ツールです。以下のオプションから表示日数を指定できます。
+株価可視化ツールです。以下のオプションから表示日数を指定できます。
 """)
 
 st.sidebar.write("""
 ## 表示日数選択
 """)
 
-days = st.sidebar.slider('日数', 1, 300, 10)
+days = st.sidebar.slider('日数', 1, 300, 60)
 
 st.write(f"""
 ### 過去 **{days}日間** のUS株価
@@ -40,8 +40,20 @@ try:
     """)
     ymin, ymax = st.sidebar.slider(
         '範囲を指定してください。',
-        0.0, 500.0, (0.0, 500.0)
+        0.0, 500.0, (30.0, 300.0)
     )
+
+#追加個所
+    st.sidebar.write("""
+    ## ratioの範囲指定
+    """)
+    ymin_ratio, ymax_ratio = st.sidebar.slider(
+        '範囲を指定してください。',
+        0.0, 2.5, (0.8, 1.3)
+    )
+
+
+#追加個所おわり
 
     tickers = {
         'nvdia': 'NVDA',
@@ -68,6 +80,31 @@ try:
         data = pd.melt(data, id_vars=['Date']).rename(
             columns={'value': 'Stock Prices(USD)'}
         )
+
+#追加個所
+        data2 = data.copy()
+        j = 0
+        for k in range(0,len(companies)): 
+            for i in range(0,days):
+                n = i + j
+                data2.loc[n,'ratio']  = data.loc[n,'Stock Prices(USD)'] / data.loc[j,'Stock Prices(USD)']
+            j = j + days
+
+        chart = (
+            alt.Chart(data2)
+            .mark_line(opacity=0.8, clip=True)
+            .encode(
+                x="Date:T",
+                y=alt.Y("ratio:Q", stack=None, scale=alt.Scale(domain=[ymin_ratio, ymax_ratio])),
+                color='Name:N'
+            )
+        )
+        st.altair_chart(chart, use_container_width=True)
+
+
+#追加個所おわり
+
+
         chart = (
             alt.Chart(data)
             .mark_line(opacity=0.8, clip=True)
